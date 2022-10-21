ffeb1b71d58dc4f91ea15fcf20fdf6a222e871d1
AndriyBarskyi <39509993+AndriyBarskyi@users.noreply.github.com>
2022-10-21 11:45:30 +0300

    add DemoDB folder

--- Pamyatajka/Pamyatajka/obj/Debug/netcoreapp3.1/MainWindow.g.cs
+++ Pamyatajka/Pamyatajka/obj/Debug/netcoreapp3.1/MainWindow.g.cs
@@ -53,7 +53,7 @@ namespace Pamyatajka {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Pamyatajka_UI;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PamyatajkaUI;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
