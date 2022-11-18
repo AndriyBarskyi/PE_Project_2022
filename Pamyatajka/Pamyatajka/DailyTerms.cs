331f534a3f731594b61aa6e54dd3190f6b05f6ae
AndriyBarskyi <39509993+AndriyBarskyi@users.noreply.github.com>
2022-11-18 10:13:57 +0200

    implement settings view

---
+++ Pamyatajka/Pamyatajka/DailyTerms.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace PamyatajkaUI
+{
+    public class DailyTerms : ObservableCollection<int>
+    {
+        public DailyTerms ()
+        {
+            Add(5);
+            Add(7);
+            Add(10);
+            Add(12);
+            Add(15);
+        }
+    }
+}
--- Pamyatajka/Pamyatajka/Views/SettingsView.xaml.cs
+++ Pamyatajka/Pamyatajka/Views/SettingsView.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace PamyatajkaUI.Views
 {
@@ -8,5 +10,11 @@ namespace PamyatajkaUI.Views
         {
             InitializeComponent();
         }
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]{2}:[^0-9]{2}");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+        
     }
 }
